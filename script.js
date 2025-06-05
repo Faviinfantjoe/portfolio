051eb6bff87e3a9a1de0b831d61db1d52d3cf340
favil infant joe.A <joefavilinfant@gmail.com>
2025-06-05 18:59:31 +0530

    f1

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Dark mode toggle
+const darkModeToggle = document.getElementById("darkModeToggle");
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if(document.body.classList.contains("dark-mode")){
+    darkModeToggle.textContent = "Light Mode";
+  } else {
+    darkModeToggle.textContent = "Dark Mode";
+  }
+});
